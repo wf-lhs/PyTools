32121c26945bf315e5c0e678a291a5dac7ad351f
wf-lhs <15flewil01@larberthighschool.co.uk>
2025-02-25 12:56:31 +0000

    Add files via upload

---
+++ PyTools-base.py
@@ -0,0 +1,9 @@
+mode = str(input("input mode:"))
+if mode == str("add"):
+    i = int(input("num1:"))
+    ii = int(input("num2:"))
+    print(i+ii)
+if mode == str("sub"):
+    i = int(input("num1:"))
+    ii = int(input("num2:"))
+    print(i-ii)
